59ae8e587051ebb8564f4c680ea1914ba49c945b
Yihong Liu <yihongliu00@gmail.com>
2018-09-03 15:25:06 -0400

    added pdf

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,3 +1,7 @@
 function githubLink(){
 	window.open("https://github.com/uurex");
 }
+
+function resumeLink(){
+	window.open("https://github.com/uurex/uurex.github.io/raw/master/assets/resume.pdf");
+}
